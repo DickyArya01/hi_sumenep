b2a14f2c06a365aa40732f83b0ed9a6ff6d1adfe
dickyarya <dickyarya170101@gmail.com>
2022-07-27 03:00:09 +0700

    model tutorial erico

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,54 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:hi_sumenep_app/user_model.dart';
 import 'package:http/http.dart' as http;
+// import 'package:hi_sumenep_app/model.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String output = 'no data';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Demo Api'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(output),
+              ElevatedButton(
+                  onPressed: () {
+                    User.getUsers("2").then((users) {
+                      output = "";
+                      for (int i = 0; i < users.length; i++){
+                        output = output +"["+ users[i].name + "]";
+
+                      }
+                      
+                      setState(() {});
+                      // output = users[i].name + ", ";
+                    });
+                  },
+                  child: const Text("GET")),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  final String apiUri = 'https://jsonplaceholder.typicode.com/albums/1';
-
-  Future<http.Response> fetchAlbum() {
-    return http.get(Uri.parse(apiUri));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
-    );
-  }
-}
-
