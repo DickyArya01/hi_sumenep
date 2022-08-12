90b789e6ba2b6efd1f9ad85041824a45f11c9e84
4nwar <sadad279@gmail.com>
2022-08-12 21:07:55 +0700

    memperbaiki tampilam card dan button category

--- lib/component/card.dart
+++ lib/component/card.dart
@@ -3,24 +3,63 @@ import 'package:hi_sumenep_app/api/dummyRepo.dart';
 import 'package:hi_sumenep_app/constant/constant.dart';
 import 'package:hi_sumenep_app/page/detail.dart';
 
-class CustomCard extends StatelessWidget {
-  final Wisata wisata;
+Container cardImage(context, String url){
+  return Container(
+                      width: visualWidth(context),
+                      height: visualHeight(context) * 0.3,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(16),
+                          topRight: Radius.circular(16),
+                        ),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            url,
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+}
 
-  CustomCard({required this.wisata});
+Container descText (String title){
+  return Container(
+                          margin: const EdgeInsets.only(
+                            left: 24,
+                            top: 4,
+                          ),
+                          child: Text(
+                            title,
+                            style: blackTextStyle.copyWith(
+                              fontSize: 20,
+                              fontWeight: bold,
+                            ),
+                          ),
+                        );
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => Detail(wisata: wisata)));
-      },
-      child: Stack(
+Container favouriteButton (){
+  return Container(
+    margin: EdgeInsets.only(right: 24, top: 4),
+    child: IconButton(
+      onPressed: () {
+        print('tambahkan ke favorit');
+        },
+        icon: Icon(
+          Icons.favorite,
+          color: Colors.grey,
+          size: 24,
+          )),
+  );
+}
+
+Stack cardItem(context, String title, String url, String desc ){
+  return Stack(
         children: [
           Container(
             margin: EdgeInsets.symmetric(vertical: x16),
-            width: 350,
-            height: 250,
+            width: visualWidth(context),
+            height: visualHeight(context) * 0.42,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(16),
@@ -38,38 +77,19 @@ class CustomCard extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      width: 350,
-                      height: 150,
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(16),
-                          topRight: Radius.circular(16),
-                        ),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            wisata.url,
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+                    cardImage(context, url),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        descText(title),
+                        favouriteButton()
+                      ],
                     ),
                     Container(
-                      margin: const EdgeInsets.only(
-                        left: 12,
-                        top: 12,
-                      ),
-                      child: Text(
-                        wisata.title,
-                        style: blackTextStyle.copyWith(
-                          fontSize: 16,
-                          fontWeight: bold,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(x16, x8, 0, 0),
-                      child: Text(wisata.desc),
+                      margin: const EdgeInsets.only(left: 24),
+                      child: Text(desc,
+                          style: greyTextStyle.copyWith(
+                              fontSize: 16, fontWeight: medium)),
                     ),
                   ],
                 ),
@@ -77,7 +97,23 @@ class CustomCard extends StatelessWidget {
             ),
           )
         ],
-      ),
+      );
+}
+
+class CustomCard extends StatelessWidget {
+  final Wisata wisata;
+
+  CustomCard({required this.wisata});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => Detail(wisata: wisata)));
+      },
+      child: cardItem(context, wisata.title, wisata.url, wisata.desc)
     );
   }
 }
+
