db7c48248a11a22aa4e4ae3a2dbcce2dd6ded5d5
dickyarya <dickyarya170101@gmail.com>
2022-08-12 21:11:10 +0700

    reformat card.dart

--- lib/component/card.dart
+++ lib/component/card.dart
@@ -5,37 +5,37 @@ import 'package:hi_sumenep_app/page/detail.dart';
 
 Container cardImage(context, String url){
   return Container(
-                      width: visualWidth(context),
-                      height: visualHeight(context) * 0.3,
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(16),
-                          topRight: Radius.circular(16),
-                        ),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            url,
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+    width: visualWidth(context),
+    height: visualHeight(context) * 0.3,
+    decoration: BoxDecoration(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(16),
+        topRight: Radius.circular(16),
+      ),
+      image: DecorationImage(
+        image: NetworkImage(
+          url,
+        ),
+        fit: BoxFit.cover,
+      ),
+    ),
                     );
 }
 
 Container descText (String title){
   return Container(
-                          margin: const EdgeInsets.only(
-                            left: 24,
-                            top: 4,
-                          ),
-                          child: Text(
-                            title,
-                            style: blackTextStyle.copyWith(
-                              fontSize: 20,
-                              fontWeight: bold,
-                            ),
-                          ),
-                        );
+    margin: const EdgeInsets.only(
+      left: 24,
+      top: 4,
+    ),
+    child: Text(
+      title,
+      style: blackTextStyle.copyWith(
+        fontSize: 20,
+        fontWeight: bold,
+      ),
+    ),
+  );
 }
 
 Container favouriteButton (){
@@ -55,49 +55,47 @@ Container favouriteButton (){
 
 Stack cardItem(context, String title, String url, String desc ){
   return Stack(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: x16),
-            width: visualWidth(context),
-            height: visualHeight(context) * 0.42,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  blurRadius: 6,
-                  offset: const Offset(1, 1),
-                ),
-              ],
+    children: [
+      Container(
+        margin: EdgeInsets.symmetric(vertical: x16),
+        width: visualWidth(context),
+        height: visualHeight(context) * 0.42,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              blurRadius: 6,
+              offset: const Offset(1, 1),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                cardImage(context, url),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    cardImage(context, url),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        descText(title),
-                        favouriteButton()
-                      ],
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 24),
-                      child: Text(desc,
-                          style: greyTextStyle.copyWith(
-                              fontSize: 16, fontWeight: medium)),
-                    ),
+                    descText(title),
+                    favouriteButton()
                   ],
                 ),
+                Container(
+                  margin: const EdgeInsets.only(left: 24),
+                  child: Text(desc,
+                      style: greyTextStyle.copyWith(
+                          fontSize: 16, fontWeight: medium)),
+                ),
               ],
             ),
-          )
-        ],
-      );
+          ],
+        ),
+      )],);
 }
 
 class CustomCard extends StatelessWidget {
